84819a946b87e5097c218a99cc0e1b2d9cf16921
yeungs <yeung.siu@citrix.com>
2015-06-09 22:53:53 -0400

    Minor documentation update

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'AWS_ms_dotnet'
 maintainer       'Citrix Systems, Inc.'
 maintainer_email 'yeung.siu@citrix.com'
 license          'All rights reserved'
-description      'Installs/Configures AWS Tools and SDK for .Net'
+description      'Installs/Configures Amazon Web Services (AWS) Tools and SDK for .Net'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports         'windows'
